303a80ed17f7a509b375af5ca37f34f1d75fa887
Redchin Daniil <redchindaniil@gmail.com>
2024-09-15 17:08:28 +0500

    Added /api/am_i_logged_in

--- src/api_endpoints.py
+++ src/api_endpoints.py
@@ -15,10 +15,28 @@ def get_yandex_login_url():
 def yandex_authorize():
     data = request.get_json()
 
-    print(data.get('access_token'))
-    print(data.get('expires_in'))
+    has_token = 'access_token' in data and data['access_token']
+    has_expiration = 'expires_in' in data and data['expires_in']
 
-    return jsonify({
-        'redirect_to': '/me',
-        'current_access_token': 'sigma-0001'
-    })
+    if has_token and has_expiration:
+        print(data.get('access_token'))
+        print(data.get('expires_in'))
+
+        return jsonify({
+            'redirect_to': '/me',
+            'current_access_token': 'sigma-0001'
+        })
+    else:
+        return jsonify({'error': 'Incorrect arguments'}), 
+
+
+
+@globals.app.route('/api/am_i_logged_in')
+def am_i_logged_in():
+    token = request.headers.get('Authorization')
+
+    if token:
+        print(token)
+        return jsonify({"message": "Yes"}), 200
+
+    return jsonify({"message": "No"}), 401
